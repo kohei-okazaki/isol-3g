6b47cf323a814c0eaf0835568ae35b759336db3c
kohei.okazaki <kohei.okazaki@dena.jp>
2017-11-01 13:52:29 +0900

    修正

--- app-manage/src/main/java/jp/co/isol/manage/service/AccountSearchService.java
+++ app-manage/src/main/java/jp/co/isol/manage/service/AccountSearchService.java
@@ -2,12 +2,17 @@ package jp.co.isol.manage.service;
 
 import jp.co.isol.manage.dto.AccountDto;
 
+/**
+ * @author kou1210hei<br>
+ * アカウント検索サービスIF<br>
+ *
+ */
 public interface AccountSearchService {
 
 	/**
-	 * ユーザIDからログイン情報を取得する
+	 * ユーザIDからアカウント情報を取得する
 	 * @param userId
-	 * @return ログイン情報
+	 * @return アカウント情報
 	 */
 	public AccountDto findAccountByUserId(String userId);
 }
