045d992fe040335733b77364bceddc5b20890a26
kohei.okazaki <kohei.okazaki@dena.jp>
2017-11-13 11:37:37 +0900

    修正

--- app-api/src/main/java/jp/co/isol/api/service/impl/HealthInfoServiceImpl.java
+++ app-api/src/main/java/jp/co/isol/api/service/impl/HealthInfoServiceImpl.java
@@ -38,6 +38,25 @@ public class HealthInfoServiceImpl implements HealthInfoService {
 
 		ApiLogger.getInstance().info(this.getClass(), "executeメソッド実行");
 
+		// リクエスト情報をDtoにつめる
+		HealthInfoDto dto = toHealthInfoDto(request);
+
+		// 登録処理を行う
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApiConfig.class);
+		HealthInfoDao dao = context.getBean(HealthInfoDao.class);
+		dao.registHealthInfo(dto);
+
+		return dto;
+	}
+
+	/**
+	 * 健康情報Dtoにリクエスト情報をつめる
+	 * @param request
+	 * @return
+	 * @throws ParseException
+	 */
+	private HealthInfoDto toHealthInfoDto(HealthInfoRequest request) throws ParseException {
+
 		ApplicationContext context = new AnnotationConfigApplicationContext(ApiConfig.class);
 		HealthInfoDao dao = context.getBean(HealthInfoDao.class);
 
@@ -48,17 +67,9 @@ public class HealthInfoServiceImpl implements HealthInfoService {
 		BigDecimal standardWeight = calcStandardWeight(CalcUtil.convertMeterFromCentiMeter(height));
 
 		// 最後に登録した健康情報を取得する
-		List<HealthInfoDto> dtoList = dao.getHealthInfoByUserId(userId);
-		HealthInfoDto lastDto = dtoList.get(dtoList.size() - 1);
-		SubKey subkey = null;
-		if (lastDto.getWeight().compareTo(weight) == 0) {
-			subkey = SubKey.EVEN;
-		} else if (lastDto.getWeight().compareTo(weight) == -1) {
-			subkey = SubKey.INCREASE;
-		} else {
-			subkey = SubKey.DOWN;
-		}
-		String userStatus = CodeManager.getInstance().getValue(MainKey.HEALTH_INFO_USER_STATUS, subkey);
+		HealthInfoDto lastDto = getLastHealthInfoDto(userId);
+
+		String userStatus = getUserStatus(weight, lastDto.getWeight());
 		Date regDate = new Date();
 
 		HealthInfoDto dto = new HealthInfoDto();
@@ -72,10 +83,40 @@ public class HealthInfoServiceImpl implements HealthInfoService {
 		dto.setUserStatus(userStatus);
 		dto.setRegDate(regDate);
 
-		// 登録処理を行う
-		dao.registHealthInfo(dto);
-
 		return dto;
+	}
+
+	/**
+	 * 入力した健康情報.体重と前回入力した健康情報.体重を比較してユーザステータスを返す<br>
+	 * @param inputWeight
+	 * @param beforeWeight
+	 * @return
+	 */
+	private String getUserStatus(BigDecimal inputWeight, BigDecimal beforeWeight) {
+		SubKey subkey = null;
+		if (beforeWeight.compareTo(inputWeight) == 0) {
+			subkey = SubKey.EVEN;
+		} else if (beforeWeight.compareTo(inputWeight) == -1) {
+			subkey = SubKey.INCREASE;
+		} else {
+			subkey = SubKey.DOWN;
+		}
+		return CodeManager.getInstance().getValue(MainKey.HEALTH_INFO_USER_STATUS, subkey);
+	}
+
+	/**
+	 * 指定されたユーザIDで最後に登録した健康情報Dtoを返す<br>
+	 * @param userId
+	 * @return
+	 * @throws ParseException
+	 */
+	private HealthInfoDto getLastHealthInfoDto(String userId) throws ParseException {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApiConfig.class);
+		HealthInfoDao dao = context.getBean(HealthInfoDao.class);
+
+		List<HealthInfoDto> dtoList = dao.getHealthInfoByUserId(userId);
+		return dtoList.get(dtoList.size() - 1);
 	}
 
 	/**
