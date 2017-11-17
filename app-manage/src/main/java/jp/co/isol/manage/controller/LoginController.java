aa759beb8505b19351ba62850dcc0eebbc8c9f2c
kohei.okazaki <kohei.okazaki@dena.jp>
2017-11-17 16:04:18 +0900

    定数値をExcelから取得するように変更

--- app-manage/src/main/java/jp/co/isol/manage/controller/LoginController.java
+++ app-manage/src/main/java/jp/co/isol/manage/controller/LoginController.java
@@ -1,7 +1,19 @@
 package jp.co.isol.manage.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -31,7 +43,17 @@ public class LoginController {
 	 */
 	@GetMapping
 	public String login(Model model, HttpServletRequest request) {
-
+		try {
+			test();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (EncryptedDocumentException e) {
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		}
 		ManageLogger logger;
 		ManageSessionManager sessionManager;
 		try (ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(ManageConfig.class)) {
@@ -43,5 +65,29 @@ public class LoginController {
 		logger.info(this.getClass(), "# login start");
 
 		return View.LOGIN.getName();
+	}
+
+	private void test() throws FileNotFoundException, IOException, EncryptedDocumentException, InvalidFormatException {
+		String targetPath = "C:\\work\\pleiades\\workspace\\isol-3g\\app-manage\\src\\main\\resources\\META-INF\\data.xlsx";
+
+		Workbook workbook = WorkbookFactory.create(new File(targetPath));
+
+		Sheet sheet = workbook.getSheet("code");
+
+		Iterator<Row> iteRow = sheet.rowIterator();
+
+		while (iteRow.hasNext()) {
+			Row row = iteRow.next();
+
+			Iterator<Cell> iteCell = row.cellIterator();
+
+			while (iteCell.hasNext()) {
+				Cell cell = iteCell.next();
+
+				System.out.println(cell.getStringCellValue());
+			}
+		}
+
+
 	}
 }
