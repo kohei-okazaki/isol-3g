19a026b5c2ba16edc1908aec5e348414f595e87e
kohei.okazaki <kohei.okazaki@dena.jp>
2017-11-17 10:20:10 +0900

    修正

---
+++ app-common/src/main/java/jp/co/isol/common/file/csv/Csv.java
@@ -0,0 +1,18 @@
+package jp.co.isol.common.file.csv;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Csvアノテーション<br>
+ * headerNamesでヘッダー名を設定<br>
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.TYPE })
+public @interface Csv {
+	String[] headerNames();
+}
