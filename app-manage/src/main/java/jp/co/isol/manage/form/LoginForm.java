f93189f71ae3f04743e0cade503d3676dee96a2c
kohei.okazaki <kohei.okazaki@dena.jp>
2017-12-14 17:47:23 +0900

    バリデータ追加

--- app-manage/src/main/java/jp/co/isol/manage/form/LoginForm.java
+++ app-manage/src/main/java/jp/co/isol/manage/form/LoginForm.java
@@ -3,6 +3,7 @@ package jp.co.isol.manage.form;
 import java.io.Serializable;
 
 import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 
 import lombok.Data;
 
@@ -15,10 +16,12 @@ public class LoginForm implements Serializable {
 
 	/** ユーザID */
 	@Max(10)
+	@NotBlank
 	private String userId;
 
 	/** パスワード */
 	@Max(10)
+	@NotBlank
 	private String password;
 
 }
