f71f5f9a97a4a75772eb8a211642b3e0883a652a
kohei-okazaki <kou1210hei@gmail.com>
2017-12-30 21:57:47 +0900

    修正

--- app-api/src/main/java/jp/co/isol/api/service/BaseApiService.java
+++ app-api/src/main/java/jp/co/isol/api/service/BaseApiService.java
@@ -6,14 +6,14 @@ import jp.co.isol.common.exception.BaseApiException;
 /**
  * 基底APIサービス<br>
  * 各機能のサービスの親クラスとする<br>
- * @param R リクエスト種別
- * @param E 例外種別
+ * @param <R> リクエスト種別
+ * @param <E> 例外種別
  */
 public interface BaseApiService<R extends BaseRequest, E extends BaseApiException> {
 
 	/**
-	 * 継承先でそれぞれチェックを実装<br>
-	 * @param T Request実装クラス
+	 * 継承先でそれぞれチェックを実装<br
+	 * @param R Request実装クラス
 	 * @throws E 例外実装クラス
 	 */
 	public void checkRequest(R request) throws E;
--- app-common/src/main/java/jp/co/isol/common/api/BaseRequestKey.java
+++ app-common/src/main/java/jp/co/isol/common/api/BaseRequestKey.java
@@ -1,12 +1,12 @@
 package jp.co.isol.common.api;
 
 /**
- * リクエストキーのマーカーインターフェース<br>
+ * リクエストキーのインターフェース<br>
  * 継承先のEnumでOverride<br>
  *
  */
 public interface BaseRequestKey {
 
-	String getValue();
+	public String getValue();
 
 }
