9d1c7918594571dc7d302b5a50abac80594d2fd6
kohei-okazaki <kou1210hei@gmail.com>
2017-12-30 18:30:55 +0900

    コンストラクタ修正

--- app-manage/src/main/java/jp/co/isol/manage/exception/LoginException.java
+++ app-manage/src/main/java/jp/co/isol/manage/exception/LoginException.java
@@ -1,6 +1,7 @@
 package jp.co.isol.manage.exception;
 
 import jp.co.isol.common.exception.BaseMvcException;
+import jp.co.isol.common.exception.ErrorCodeDefine;
 
 /**
  * ログイン画面のExceptionクラス<br>
@@ -12,7 +13,7 @@ public class LoginException extends BaseMvcException {
 	 * コンストラクタ<br>
 	 * @param errorMessage
 	 */
-	public LoginException(String errorMessage) {
-		this.errorMessage = errorMessage;
+	public LoginException(ErrorCodeDefine errorCode, String errorMessage) {
+		super(errorCode, errorMessage);
 	}
 }
