d44a71a6a8d6b1fbd3ffbf9980435fc80dfb4472
kohei-okazaki <kou1210hei@gmail.com>
2018-01-03 14:42:52 +0900

    javadoc修正

--- app-manage/src/main/java/jp/co/isol/manage/form/NoticeSettingForm.java
+++ app-manage/src/main/java/jp/co/isol/manage/form/NoticeSettingForm.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import jp.co.isol.common.web.mvc.BaseForm;
 
 /**
- * 通知設定コントローラクラス<br>
+ * 通知設定フォームクラス<br>
  *
  */
 public class NoticeSettingForm extends BaseForm implements Serializable {
