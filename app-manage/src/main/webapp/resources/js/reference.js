d772ef5000184dc406b1bd2368f53855a7638e8d
k-okazaki <kou1210hei@gmail.com>
2018-03-14 11:42:01 +0900

    Update reference.js

--- app-manage/src/main/webapp/resources/js/reference.js
+++ app-manage/src/main/webapp/resources/js/reference.js
@@ -1,3 +1,10 @@
+function referenceExcelDownload() {
+	var form = document.createElement('form');
+	form.action = '/isol-manage/result-reference-excelDownload.html';
+	form.method = 'get';
+	document.body.appendChild(form);
+	form.submit();
+}
 function referenceCsvDownload() {
 	var form = document.createElement('form');
 	form.action = '/isol-manage/result-reference-csvDownload';
