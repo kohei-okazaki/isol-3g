f010c749dc8dea673d81e8142ca73727500064bb
kohei-okazaki <kou1210hei@gmail.com>
2018-04-09 19:16:40 +0900

    ※※※修正※※※

--- app-manage/src/main/webapp/resources/js/accountCreate.js
+++
@@ -1,8 +0,0 @@
-function accountCreate() {
-	var form = document.createElement('form');
-	form.action = '/isol-web/account-create-input.html';
-	form.method = 'get';
-	document.body.appendChild(form);
-	form.submit();
-}
-
--- app-manage/src/main/webapp/resources/js/healthInfo.js
+++
@@ -1,8 +0,0 @@
-function healthInfoCsvDownload() {
-	var form = document.createElement('form');
-	form.action = '/isol-web/healthInfo-csvDownload';
-	form.method = 'get';
-	document.body.appendChild(form);
-	form.submit();
-}
-
